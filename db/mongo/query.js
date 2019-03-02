ad1f72ffd02c8389063f46e41add2b5bbaaa5970
peter <peterliu1991@gmail.com>
2019-03-02 08:52:36 -0800

    minor tab spacing

--- db/mongo/query.js
+++ db/mongo/query.js
@@ -10,26 +10,26 @@ value = field === '_id' ? mongoose.Types.ObjectId(value) : value;
 const CONNECTION_URI = process.env.MONGODB_URI || 'mongodb://localhost/sdc2';
 
 mongoose
-.connect(CONNECTION_URI, {
-  useNewUrlParser: true,
-  poolSize: 10
-})
-.then(() => {
-  const time = Date.now();
-  Listing.find({ [field]: value })
-  .then(res => {
-    console.log(res);
-    fs.writeFileSync('./db/mongo/output.json', JSON.stringify(res, null, 2), err => {
-      if (err) {
-        console.log(err);
-        process.exit();
-      }
+  .connect(CONNECTION_URI, {
+    useNewUrlParser: true,
+    poolSize: 10
+  })
+  .then(() => {
+    const time = Date.now();
+    Listing.find({ [field]: value })
+    .then(res => {
+      console.log(res);
+      fs.writeFileSync('./db/mongo/output.json', JSON.stringify(res, null, 2), err => {
+        if (err) {
+          console.log(err);
+          process.exit();
+        }
+      });
+      console.log(`found ${res.length} item${res.length > 1 ? 's' : ''} in ${Date.now() - time} ms`);
+      process.exit();
     });
-    console.log(`found ${res.length} item${res.length > 1 ? 's' : ''} in ${Date.now() - time} ms`);
+  })
+  .catch(err => {
+    console.log(err);
     process.exit();
   });
-})
-.catch(err => {
-  console.log(err);
-  process.exit();
-});
